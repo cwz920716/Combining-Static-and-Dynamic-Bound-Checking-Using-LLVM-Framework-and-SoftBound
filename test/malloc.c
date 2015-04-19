9fce7a53557ec8fe8770b0220494ad8259bf5f43
cwz920716 <cwz920716@gmail.com>
2015-04-19 17:59:46 -0500

    clean Apr 19

--- test/malloc.c
+++ test/malloc.c
@@ -14,15 +14,18 @@ struct st {
 
 int f(int s) {
 	void *ptr = malloc(sizeof(struct st) * s);
+	free(ptr + 1);
 	return s;
 }
 
 int main(int argc, char **argv) {
 	int argc2 = 10;
-	int A[10], sum = 0;
+	int A[10] = {0}, sum = 0;
 	for (int i = 0; i < argc2; i++) {
-		A[i] = f(i);
-		sum += A[i];
+		A[i] = i;
+		char B[i+1];
+		B[i] = f(A[i]);
+		sum += B[i] + A[i];
 	}
 	return f(sum);
 }
---
+++ ubench/link.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/mtrack.h"
+
+#define T int
+
+struct Node;
+typedef struct Node *node_ptr;
+typedef struct Node node_t;
+
+struct Node {
+	T item;
+	node_ptr next;
+};
+
+node_t head;
+
+int hasNext(node_ptr cur) {
+	return cur->next != NULL;
+}
+
+node_ptr next(node_ptr cur) {
+	return cur->next;
+}
+
+struct Node copy(node_ptr cur) {
+	return (*cur);
+}
+
+void push(node_ptr cur, node_ptr n) {
+	n->next = cur->next;
+	cur->next = n;
+}
+
+node_ptr newNode(T item) {
+	node_ptr n = (node_ptr) malloc(sizeof(node_t));
+	n->item = item;
+	return n; 
+}
+
+node_ptr removeFrom(node_ptr n) {
+	if (!hasNext(n))
+		return NULL;
+
+	node_ptr ret = n->next;
+	n->next = ret->next;
+	return ret;
+}
+
+void freeNode(node_ptr n) {
+	free(n);
+}
+	
+int main() {
+	long ans[10][2];
+	int i = 0; 
+	node_ptr head_ptr = &head;
+
+	for (i = 0; i < 10; i++) {
+		ans[i][0] = i;
+		node_ptr n = newNode(i);
+		ans[i][1] = (long) n;
+		push(head_ptr, n);
+	}
+
+	while (hasNext(head_ptr)) {
+		node_ptr np = removeFrom(head_ptr);
+		struct Node n = copy(np);
+		printf("--%d--\n", n.item);
+		freeNode(np);
+	}
+
+	return 0;
+}
